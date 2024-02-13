d5db541f6aa5db92ef100cce152ed2b6f0192994
Maxel <carlosururchurtu@gmail.com>
2024-02-13 12:29:09 -0700

    se agregaron labels

--- Proyecto1/Registro_User.cs
+++ Proyecto1/Registro_User.cs
@@ -5,6 +5,10 @@ namespace Proyecto1
 {
     internal class Registro_User : Form
     {
+        private Label label1;
+        private Label label2;
+        private Label label3;
+
         internal static void show()
         {
             throw new NotImplementedException();
@@ -13,16 +17,50 @@ namespace Proyecto1
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registro_User));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(677, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
             // 
             // Registro_User
             // 
             this.ClientSize = new System.Drawing.Size(945, 506);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Registro_User";
             this.Text = "Registro";
             this.Load += new System.EventHandler(this.Registro_User_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
